ffc43768c4b855c6588726b41deec4b7282d0757
Livia Brasil <liviabrasil@id.uff.br>
2021-04-05 03:54:45 -0300

    função selectfood pronta

--- scripts.js
+++ scripts.js
@@ -1,56 +1,63 @@
+let foodcost=0;
+let drinkcost=1;
+let dessertcost=2;
+var foodname;
+var drinkname;
+var dessertname;
+
 function selectfood(foodselected) {
 
-    // tirar verde de quem ta verde //
-    console.log("entrei na função");
     const selectedfood = document.querySelector(".selectedfood");
+    const icon = document.querySelector(".show");
+    const selector = "." + "dish" + foodselected;
+    const selectoricon = "." + "icon" + foodselected;
+    const button = document.querySelector(selector);
+    const buttonicon = document.querySelector(selectoricon);
+    
 
     if (selectedfood !== null) {
-    console.log("vou remover o selecionado")
+    console.log("vou remover o selecionado");
     selectedfood.classList.remove('selectedfood');
+    icon.classList.remove('show');
     }
 
-    // botar verde em quem foi clicado //
-    const selector = "." + foodselected;
-    const button = document.querySelector(selector);
-    console.log("adicionei selecionado no clicado")
+    foodcost = parseFloat(document.getElementById("foodcost"+foodselected).innerText);
+    foodname = document.getElementById("foodname"+foodselected).innerText;
 
     button.classList.add('selectedfood');
+    buttonicon.classList.add('show');
+
+    checkout ();
 }
 
 function selectdrink(drinkselected) {
 
-    // tirar verde de quem ta verde //
-    console.log("entrei na função");
     const selecteddrink = document.querySelector(".selecteddrink");
+    const icon = document.querySelector(".show");
+    const selector = "." + "dish" + drinkselected;
+    const selectoricon = "." + "icon" + drinkselected;
+    const button = document.querySelector(selector);
+    const buttonicon = document.querySelector(selectoricon);
+    
 
     if (selecteddrink !== null) {
-    console.log("vou remover o selecionado")
+    console.log("vou remover o selecionado");
     selecteddrink.classList.remove('selecteddrink');
+    icon.classList.remove('show');
     }
 
-    // botar verde em quem foi clicado //
-    const selector = "." + drinkselected;
-    const button = document.querySelector(selector);
-    console.log("adicionei selecionado no clicado")
+    drinkcost = parseFloat(document.getElementById("drinkcost"+drinkselected).innerText);
+    drinkname = document.getElementById("drinkname"+drinkselected).innerText;
 
     button.classList.add('selecteddrink');
+    buttonicon.classList.add('show');
+
+    checkout ();
 }
 
-function selectdessert(dessertselected) {
-
-    // tirar verde de quem ta verde //
-    console.log("entrei na função");
-    const selecteddessert = document.querySelector(".selecteddessert");
-
-    if (selecteddessert !== null) {
-    console.log("vou remover o selecionado")
-    selecteddessert.classList.remove('selecteddessert');
+function checkout (){
+    if(foodcost!==0 && drinkcost!==0 && dessertcost!==0){
+        const buttonCheckout = document.querySelector(".button-checkout");
+        buttonCheckout.classList.add('button-checkout-on');
     }
-
-    // botar verde em quem foi clicado //
-    const selector = "." + dessertselected;
-    const button = document.querySelector(selector);
-    console.log("adicionei selecionado no clicado")
-
-    button.classList.add('selecteddessert');
 }
